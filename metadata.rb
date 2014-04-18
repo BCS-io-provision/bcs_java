35bb42701dca2cc08eace9876922fb33ad3f6208
richardwigley <richard.wigley@gmail.com>
2014-04-18 10:52:58 +0000

    berks v3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'richard.wigley@github.com'
 license          'All rights reserved'
 description      'Installs/Configures bcs_java'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '1.0.0'
 
 depends 'java'
 depends 'apt'
