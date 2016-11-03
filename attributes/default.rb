d84fa577b36dd0ee0e4d6659200b3909aade063d
Richard <richard.wigley@gmail.com>
2016-11-03 20:31:26 +0000

    Allow for Ubuntu 14 and 16 this time ifs

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,5 @@
 
-case
-when node['platform_version'].to_f <= 14.04
+if node['platform_version'].to_f <= 14.04
   default['java']['package'] = 'openjdk-7-jre-headless'
 else
   default['java']['package'] = 'openjdk-8-jre-headless'
