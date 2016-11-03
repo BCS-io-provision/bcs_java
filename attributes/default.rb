c5f8f83bc094055c85a54073136b74d6bb7463c8
Richard <richard.wigley@gmail.com>
2016-11-03 18:41:29 +0000

    Allow for Ubuntu 14 and 16

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,7 @@
 
-default['java']['package'] = 'openjdk-8-jre-headless'
+case
+when node['platform_version'].to_f <= 14.04
+  default['java']['package'] = 'openjdk-7-jre-headless'
+else
+  default['java']['package'] = 'openjdk-8-jre-headless'
+end
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'richard.wigley@github.com'
 license          'All rights reserved'
 description      'Installs/Configures bcs_java'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.0'
+version          '3.0.1'
 
 depends 'apt'
