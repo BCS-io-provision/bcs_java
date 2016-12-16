a5cbacf661997789e6b5215661e6f3ec11f7302d
Richard <richard.wigley@gmail.com>
2016-12-16 17:37:07 +0000

    Reorganise metadata like other cookbooks

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,11 @@ description      'Installs/Configures bcs_java'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.0.5'
 
+source_url       'https://github.com/BCS-io-provision/bcs_java' if respond_to?(:source_url)
+issues_url       'https://github.com/BCS-io-provision/bcs_java/issues' if respond_to?(:issues_url)
+
 depends 'apt'
 
 %w(ubuntu).each do |os|
   supports os
 end
-
-source_url       'https://github.com/BCS-io-provision/bcs_java' if respond_to?(:source_url)
-issues_url       'https://github.com/BCS-io-provision/bcs_java/issues' if respond_to?(:issues_url)
