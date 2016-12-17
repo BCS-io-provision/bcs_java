0e7cb101df44a9b9f05ec4394b9deb908e7fbda8
Richard <richard.wigley@gmail.com>
2016-12-17 10:43:20 +0000

    Rubocoped

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,8 +2,8 @@
 # Ubuntu 14 expects openjdk-7
 # Ubuntu 16 expects openjdk-8
 #
-if node['platform_version'].to_f <= 14.04
-  default['java']['package'] = 'openjdk-7-jre-headless'
-else
-  default['java']['package'] = 'openjdk-8-jre-headless'
-end
+default['java']['package'] = if node['platform_version'].to_f <= 14.04
+                               'openjdk-7-jre-headless'
+                             else
+                               'openjdk-8-jre-headless'
+                             end
