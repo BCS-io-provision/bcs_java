db776734036c945a47be19d7afbe5b8a7d2576c2
Richard <richard.wigley@gmail.com>
2017-04-30 11:36:04 +0100

    metadata updated
      - license updated to a standard one
      - chef_version required

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'bcs_java'
 maintainer       'Richard Wigley '
 maintainer_email 'richard.wigley@github.com'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs/Configures bcs_java'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.1.0'
@@ -11,6 +11,8 @@ issues_url       'https://github.com/BCS-io-provision/bcs_java/issues' if respon
 
 depends 'apt'
 
-%w(ubuntu).each do |os|
+%w[ubuntu].each do |os|
   supports os
 end
+
+chef_version '>= 12.5' if respond_to?(:chef_version)
