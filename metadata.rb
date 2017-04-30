bbcebd4e537c489423dd749d180d2799f622bb93
Richard <richard.wigley@gmail.com>
2017-04-30 11:41:45 +0100

    Version bump 3.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'richard.wigley@github.com'
 license          'MIT'
 description      'Installs/Configures bcs_java'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.1.0'
+version          '3.2.0'
 
 source_url       'https://github.com/BCS-io-provision/bcs_java' if respond_to?(:source_url)
 issues_url       'https://github.com/BCS-io-provision/bcs_java/issues' if respond_to?(:issues_url)
